c232dbaf75109ea30a31b1cc7bdcec958aab8958
maodzhedun <48294837+maodzhedun@users.noreply.github.com>
2025-07-14 12:17:07 +0300

    test commit Versel

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + TS Cafe</h1>
+      <h1>Vite + React + TS with Vercel</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
